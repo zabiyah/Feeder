986872a1647cb90704bef3415d3fcd07558ffe2b
zabiyah <zabiyahk@nycstudents.net>
2024-10-09 11:44:03 -0400

    Add files via upload

--- Feeder.java
+++ Feeder.java
@@ -13,7 +13,15 @@ public class Feeder {
     public void simulateOneDay(int numBirds)
     {
         if(Math.random() < 0.05) currentFood = 0;
-    }
+        else {
+            int eaten = (int)(Math.random() * 41) + 10;
+            currentFood -= eaten * numBirds;
+            if(currentFood < 0) currentFood = 0;
+        }
+
+
+        }
+    
 
     /**
      * Returns the number of days birds or a bear found food to eat at the
@@ -22,7 +30,17 @@ public class Feeder {
      */
     public int simulateManyDays(int numBirds, int numDays) 
     {
-        return 0;
+        int days = 0;
+        while (numDays > 0)
+        { 
+            if (currentFood==0) return days;
+            simulateOneDay(numBirds);
+            numDays--;
+            days++;
+        } 
+       
+        return days;
+       
     }
     public int getCurrentFood()
     {
@@ -33,5 +51,11 @@ public class Feeder {
         currentFood = c;
     }
     public Feeder() {}
+    
+    public String toString()
+    {
+        return "Current Food: " + currentFood;
+    }
+
 
 }
--- Main.java
+++ Main.java
@@ -18,5 +18,29 @@ public class Main
         System.out.println(Math.random() < 0.05);  
         g.simulateOneDay(12);
         System.out.println(g.getCurrentFood());
-    }
+        Feeder h = new Feeder(1000);
+        h.simulateOneDay(22);
+        System.out.println(h.getCurrentFood());
+        Feeder i = new Feeder(100);
+        i.simulateOneDay(5);
+        System.out.println(i);
+        Feeder j = new Feeder(2400);
+        System.out.println(j);
+        System.out.println(j.simulateManyDays(10, 4));
+        System.out.println("Test case 1");
+        System.out.println(j);
+        System.out.println(j.simulateManyDays(10, 4));
+        System.out.println(j);
+        System.out.println();
+        System.out.println("Test case 2");
+        Feeder k = new Feeder (250);
+        System.out.println(k);
+        System.out.println(k.simulateManyDays(10, 5));
+        System.out.println();
+        System.out.println("Test case 3 !");
+        Feeder l = new Feeder(0);
+        System.out.println(l);
+        System.out.println(l.simulateManyDays(5, 10));
+
+    } 
 }
